0ad1632ef26e19b1528dd5f900349028068d06bf
Sebastian Mora <sebastian.mora3@udea.edu.co>
2023-11-07 00:12:23 -0500

    Importacion del modulo: react-boostrap, Creaccion de barra de Navegacion

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import React, { Fragment } from "react";
 import { createRoot } from 'react-dom/client';
 
 import List from "./containers/List";
-import Navbar from "./components/Navbar";
+import NavbarMain from "./components/Navbar";
 import Footer from "./components/Footer/footer";
 
 import "bootswatch/dist/lux/bootstrap.min.css";
 import 'bootstrap/dist/css/bootstrap.css';
 import "./index.css";
 
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Info from "./components/Footer/info";
 import Ayuda from "./components/Footer/ayuda";
 import Detail from "./components/Detail";
@@ -17,12 +17,13 @@ import Detail from "./components/Detail";
 const App = () => {
   return (
       <BrowserRouter>
-          <Navbar />
+          <NavbarMain/>
           <Routes className="bg-dark">
                 <Route path="/" element={<List/>}/>
                 <Route path="/info" element={<Info/>}/>
                 <Route path="/ayuda" element={<Ayuda/>}/>
                 <Route path="/detail/:id" element={<Detail/>}/>
+                <Route path="*" element={<Navigate replace to="/"/>}/>
           </Routes>
           <Footer/>
       </BrowserRouter>
