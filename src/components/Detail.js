9e410c482407ef7bc371f888d5dfd4f4b3e8f194
Juan Loaiza <juanmazo1234@gmail.com>
2023-11-13 21:58:12 -0500

    se modifica la drecipcion

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -6,7 +6,6 @@ const API = process.env.API;
 const Detail = () => {
 
     const { id } = useParams();
-    console.log(id);
 
     const [movie, setMovie] = useState({});
 
@@ -21,11 +20,20 @@ const Detail = () => {
     }
 
     return (
-        <div>
-            <h1>{movie.Title}</h1>
-            <p>{movie.Country}</p>
-            <p>{movie.Plot}</p>
-            <img src={movie.Poster} alt={movie.Title}/>
+        <div className="text-light d-flex justify-content-center row">
+            <div className="text-center fs-4 fw-bolder p-0 text-uppercase">
+                <p className="text-center text-info m-3">{movie.Title}</p>
+            </div>
+            <div className="d-flex justify-content-center">
+                <div className="justify-content-center col-md-2 m-5 row">
+                    <img src={movie.Poster} alt={movie.Title} className="card-img-top" width="100"/>
+                    <p className="text-center">  Año: {movie.Year}</p>
+                </div>
+                <div className="justify-content-center col-md-2 m-5 row">
+                    <p>  Pais: {movie.Country}</p>
+                    <p>  Sinopsis: {movie.Plot}</p>
+                </div>
+            </div>
         </div>
     )
 }
